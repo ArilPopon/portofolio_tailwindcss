aa33508479df571b6e53c993108abdb41a63a387
ArilPopon <arilponconugroho@gmail.com>
2024-03-12 11:13:16 +0700

    feat: dark mode & fix some view&feat

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const backToTop = document.querySelector("#to-top");
 
   if (window.scrollY > fixedNav) {
     header.classList.add("navbar-fixed");
+    backToTop.classList.remove("hidden");
+    backToTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    backToTop.classList.remove("flex");
+    backToTop.classList.add("hidden");
   }
 };
 
@@ -18,3 +23,37 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// Click Outside Hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// Move Toggle Based On Mode
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
